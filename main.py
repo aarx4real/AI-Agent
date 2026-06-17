fa807fb97bedb8f7d074007876b63f6de81551a5
aarx4real <aarushgupt29@gmail.com>
2026-06-17 21:19:55 +0530

    Added OpenAI chat functionality

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def main():
-    print("Hello from project1!")
+    model = ChatOpenAI(temperature=0)
 
+    tools = []
+    agent_executer = create_react_agent(models, tools)
 
-if __name__ == "__main__":
+    print("Welcome! I am your AI assistant, type 'quit' to exit.")
+    print("How can I help you Today.")
+
+    while True:
+        user_input = input("/nYou: ").strip()
+
+        if user_input=="quit":
+            break
+        
+        print("\nAssistant: ",end="")
+        for chunk in agent_executer.stream(
+            {"messages" : [HumanMessage(content=user_input)]}
+        ):
+
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+            print()
+
+if __name__ += "__main__":
     main()
